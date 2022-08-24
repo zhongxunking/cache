5c7c1c90b987e308c0e6afbcaa2884b0ed173e22
zhongxunking <zhongxunking@163.com>
2022-08-25 00:44:38 +0800

    缓存一致性方案5：实现解写锁lua脚本

--- src/main/resources/META-INF/consistencyv5/locker/sync/RWLock-unlockForWrite.lua
+++ src/main/resources/META-INF/consistencyv5/locker/sync/RWLock-unlockForWrite.lua
@@ -1,9 +1,10 @@
 -- KEYS: lockKey
--- ARGV: lockerId, syncChannel
+-- ARGV: lockerId, syncChannel, value, valueLiveTime
 -- return: true（成功）；false（失败，锁不存在或已经易主）
 
 -- 数据结构（hash）
 -- ${lockKey}:
+--   value: ${value}
 --   owner: none、writer、readers、reader-writer
 --   writerBooking: ${writerBooking}
 --   writer: ${lockerId}
@@ -16,6 +17,11 @@
 local lockKey = KEYS[1];
 local lockerId = ARGV[1];
 local syncChannel = ARGV[2];
+local value = ARGV[3];
+local valueLiveTime = ARGV[4];
+if (valueLiveTime ~= nil) then
+    valueLiveTime = tonumber(valueLiveTime);
+end
 -- 获取owner
 local owner = redis.call('hget', lockKey, 'owner');
 if (owner == false) then
@@ -30,17 +36,33 @@ if (owner == 'writer' or owner == 'reader-writer') then
         -- 删除writer
         writer = false;
         redis.call('hdel', lockKey, 'writer');
+        -- 更新owner
         if (owner == 'writer') then
-            -- 删除锁
-            redis.call('del', lockKey);
+            owner = 'none';
         else
-            -- 更新owner
             owner = 'readers';
-            redis.call('hset', lockKey, 'owner', owner);
         end
+        redis.call('hset', lockKey, 'owner', owner);
+        -- 判断是否删除key
+        if (owner == 'none') then
+            -- 如无value，则删除key
+            local existingValue = redis.call('hget', lockKey, 'value');
+            if (existingValue == false) then
+                redis.call('del', lockKey);
+            end
+        end
+
         success = true;
     end
 end
 -- 发布同步消息
 redis.call('publish', syncChannel, 0);
+-- 设置value
+if (success == true and value ~= nil) then
+    redis.call('hset', lockKey, 'value', value);
+    if (owner == 'none' and valueLiveTime ~= nil) then
+        redis.call('pexpire', lockKey, valueLiveTime);
+    end
+end
+
 return success;
